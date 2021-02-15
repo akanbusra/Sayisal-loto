03807926289dddc42cffc5ac2337c175e7822100
Büşra Akan <78661692+akanbusra@users.noreply.github.com>
2021-02-15 21:20:56 +0300

    Sayısal loto projesi
    Rastgele gireceğimiz sayıların doğru olup olmadığını kontrol eden bir sayısal loto projesidir

---
+++ sayısal_loto_uygulaması/sayısal_loto_uygulaması/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayısal_loto_uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int s1, s2, s3, s4, s5, s6;
+            s1 = rnd.Next(1, 4);
+            s2 = rnd.Next(1, 4);
+            s3 = rnd.Next(1, 4);
+            s4 = rnd.Next(1, 4);
+            s5 = rnd.Next(1, 4);
+            s6 = rnd.Next(1, 4);
+
+            label1.Text = s1.ToString();
+            label2.Text = s2.ToString();
+            label3.Text = s3.ToString();
+            label4.Text = s4.ToString();
+            label5.Text = s5.ToString();
+            label6.Text = s6.ToString();
+
+            //sayı1
+            if (label1.Text==textBox1.Text)
+            {
+                textBox1.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox1.BackColor = Color.Red;
+            }
+
+            //sayı2
+            if (label2.Text == textBox2.Text)
+            {
+                textBox2.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox2.BackColor = Color.Red;
+            }
+
+            //sayı3
+            if (label3.Text == textBox3.Text)
+            {
+                textBox3.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox3.BackColor = Color.Red;
+            }
+
+            //sayı4
+            if (label4.Text == textBox4.Text)
+            {
+                textBox4.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox4.BackColor = Color.Red;
+            }
+
+            //sayı5
+            if (label5.Text == textBox5.Text)
+            {
+                textBox5.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox5.BackColor = Color.Red;
+            }
+
+            //sayı6
+            if (label6.Text == textBox6.Text)
+            {
+                textBox6.BackColor = Color.Green;
+            }
+            else
+            {
+                textBox6.BackColor = Color.Red;
+            }
+
+        }
+    }
+}
